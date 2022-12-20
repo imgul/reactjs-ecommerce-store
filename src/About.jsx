5e5343677e02cf2d59b7b37b202cbe252029fe57
Gul Zaib <bc210411296mgu@vu.edu.pk>
2022-12-20 20:16:45 +0500

    setup context api and hooks

--- src/About.jsx
+++ src/About.jsx
@@ -1,12 +1,20 @@
-import React from "react";
+import { React, useContext } from "react";
 import HeroSection from "./components/HeroSection";
+import { AppContext } from "./context/productsContext";
 
 const About = () => {
+  const { author } = useContext(AppContext);
+
   const data = {
     name: "Thapa Ecommerce",
   };
 
-  return <HeroSection myData={data} />;
+  return (
+    <>
+      {author.name}
+      <HeroSection myData={data} />
+    </>
+  );
 };
 
 export default About;
--- src/context/productsContext.js
+++ src/context/productsContext.js
@@ -2,6 +2,14 @@ import { createContext } from "react";
 
 const AppContext = createContext();
 
-const AppProvider = ({children}) => {
-    return <AppContext.Provider value={authorName}
+const AppProvider = ({ children }) => {
+  const author = {
+    name: "Gulzaib",
+    address: "Lahore",
+  };
+  return (
+    <AppContext.Provider value={{ author }}>{children}</AppContext.Provider>
+  );
 };
+
+export { AppProvider, AppContext };
