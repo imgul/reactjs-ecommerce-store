53ed06f6cc0f8a92fbb77fa5393d8de104bc226d
Gul Zaib <bc210411296mgu@vu.edu.pk>
2022-12-20 18:10:21 +0500

    contact page

--- src/Contact.js
+++ src/Contact.js
@@ -4,23 +4,18 @@ const Contact = () => {
   const Wrapper = styled.section`
     padding: 9rem 0 5rem 0;
     text-align: center;
-
     .container {
       margin-top: 6rem;
-
       .contact-form {
         max-width: 50rem;
         margin: auto;
-
         .contact-inputs {
           display: flex;
           flex-direction: column;
           gap: 3rem;
-
           input[type="submit"] {
             cursor: pointer;
             transition: all 0.2s;
-
             &:hover {
               background-color: ${({ theme }) => theme.colors.white};
               border: 1px solid ${({ theme }) => theme.colors.btn};
@@ -33,7 +28,59 @@ const Contact = () => {
     }
   `;
 
-  return <Wrapper>Contact</Wrapper>;
+  return (
+    <Wrapper>
+      <h2 className="common-heading">Contact page</h2>
+
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d93884.29911395!2d74.26337326362709!3d31.539456518662476!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39191ca8f5a906f1%3A0xc5dad8adc056fa92!2sLahore%20Museum!5e1!3m2!1sen!2s!4v1671541706810!5m2!1sen!2s"
+        width="100%"
+        height="400"
+        style={{ border: 0 }}
+        title="Gulzaib Store Info"
+        allowFullScreen=""
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"
+      ></iframe>
+
+      <div className="container">
+        <div className="contact-form">
+          <form
+            action="https://formspree.io/f/xeqdgwnq"
+            method="POST"
+            className="contact-inputs"
+          >
+            <input
+              type="text"
+              placeholder="username"
+              name="username"
+              required
+              autoComplete="off"
+            />
+
+            <input
+              type="email"
+              name="Email"
+              placeholder="Email"
+              autoComplete="off"
+              required
+            />
+
+            <textarea
+              name="Message"
+              cols="30"
+              rows="10"
+              required
+              autoComplete="off"
+              placeholder="Enter you message"
+            ></textarea>
+
+            <input type="submit" value="send" />
+          </form>
+        </div>
+      </div>
+    </Wrapper>
+  );
 };
 
 export default Contact;
